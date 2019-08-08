aae1558bfed1dabc5a6719b169f9462041c60761
Sanjeev S <sanjeevs@thoughtworks.com>
2019-08-09 00:15:55 +0530

    Configuring EngineMain and app config

--- src/main/kotlin/edu/workshop/kotlin/game/App.kt
+++ src/main/kotlin/edu/workshop/kotlin/game/App.kt
@@ -4,15 +4,14 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("<h2>Cool!! You are all set for the workshop \uD83D\uDC4D</h2>", ContentType.Text.Html)
-            }
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("<h2>Cool!! You are all set for the workshop \uD83D\uDC4D</h2>", ContentType.Text.Html)
         }
-    }.start(wait = true)
+    }
 }
+
+
